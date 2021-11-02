3bec177e09c3ac6003054dfc5e62c197783ad3f3
jarrett-taylor-repos <91221932+jarrett-taylor-repos@users.noreply.github.com>
2021-11-01 21:27:41 -0500

    initial commit
    holds all old code and bit chess folder

---
+++ ChessAi/optimize/speed.cpp
@@ -0,0 +1,62 @@
+#include "Board.cpp"
+#include <chrono>
+using namespace std::chrono;
+#include <time.h>
+
+int moveGenerationTest(Board b, int depth, ofstream& myfile) {
+    if(depth == 0) {
+        return 1;
+    }
+    int numPos = 0;
+    vector<pair<Notation, Notation>> notmoves = b.returnNotationMoves();
+    for(int i = 0; i < notmoves.size(); i++) {
+        pair<Notation, Notation> tempmove = notmoves[i];
+        //Board temp(b.getFEN());
+        Board temp = b;
+        bool mademove = temp.makeMove(tempmove.first, tempmove.second);
+        if(mademove) {
+            myfile << temp.getFEN() << " NUMMOVES: " << temp.returnNotationMoves().size() << endl;
+            numPos += moveGenerationTest(temp, depth-1, myfile);
+        } else {
+            cout << temp.getFEN() << " attempted move " << tempmove.first << tempmove.second << endl;
+        }
+    }
+    return numPos;
+}
+
+
+int main() {
+    string testPos1 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    Board b(testPos1);
+    // 1 - 44
+    // 2 - 1,486
+    // 3 - 62,379
+    // 4 - 2,103,487
+    // 5 - 89,941,194
+
+    //Board b;
+    // 1	20
+    // 2	400
+    // 3	8,902
+    // 4	197,281
+    // 5	4,865,609
+    // 6	119,060,324
+    // 7	3,195,901,860
+    // 8	84,998,978,956
+    // 9	2,439,530,234,167
+    // 10	69,352,859,712,417
+    ofstream myfile;
+    myfile.open("chessPositions.txt");
+    int depth = 4;
+    int nummoves = 0;
+    clock_t tStart = clock();
+    cout << "Depth - num" << endl;
+    for(int i = 1; i <= depth; i++) {
+        nummoves =  moveGenerationTest(b, i, myfile);
+        cout << i << " - " << nummoves << endl;
+        printf("Time taken: %.4fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
+    }
+    myfile.close();
+    return 0;
+}
---
+++ ChessAi/speed/zstuff.cpp
@@ -0,0 +1,15 @@
+#include "Board.cpp"
+
+class zstuff{
+    public:
+    long long int zval;
+    int materialadv;
+    //Board b;
+    zstuff(long long int zval,int materialadv);
+};
+
+zstuff::zstuff(long long int zvall,int materialadvv){
+    zval = zvall;
+    materialadv = materialadvv;
+    //b = bb;
+}
